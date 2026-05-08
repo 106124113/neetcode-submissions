6c52ba0391278e99ccbaa35f1e3bf5bb71e6314b
106124113 <shaharchi868@gmail.com>
2026-05-09 04:14:57 +0530

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        map<char,int>mp1,mp2;
+        int n=s.length();
+        int m=t.length();
+        if(n!=m) return false;
+        for(int i=0;i<n;i++){
+            mp1[s[i]]++;
+            mp2[t[i]]++;
+        }
+        if(mp1==mp2) return true;
+        return false;
+    }
+};

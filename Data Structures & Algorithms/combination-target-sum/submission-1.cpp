4172b4ee8809f188b873c2beb5b7f0fbdf7da5f6
106124113 <shaharchi868@gmail.com>
2026-05-26 06:34:04 +0530

    Add: combination-target-sum - submission-1

---
+++ Data Structures & Algorithms/combination-target-sum/submission-1.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    void fxn(int i,
+             int n,
+             vector<int>& nums,
+             int target,
+             vector<vector<int>>& ans,
+             vector<int>& temp)
+    {
+        // base case
+        if(target == 0){
+            ans.push_back(temp);
+            return;
+        }
+        if(i == n) return;
+        if(nums[i] <= target){
+            temp.push_back(nums[i]);
+
+            fxn(i, n, nums, target - nums[i], ans, temp);
+
+            temp.pop_back(); // backtrack
+        }
+        // not take
+        fxn(i + 1, n, nums, target, ans, temp);
+    }
+    vector<vector<int>> combinationSum(vector<int>& nums, int target) {
+        vector<vector<int>> ans;
+        vector<int> temp;
+
+        fxn(0, nums.size(), nums, target, ans, temp);
+
+        return ans;
+    }
+};
